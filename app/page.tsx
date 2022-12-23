b53e695fcd3abaa70f6143352102e8758e4b12c9
Lukas Wiesehan <mail@lukaswiesehan.de>
2022-12-23 21:19:56 +0100

    Make headings responsive

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,25 @@ export default function Home() {
       <Section className="pt-24">
         <Heading level={1} className="max-w-4xl">
           Präsentiere Dein SaaS Produkt
-          <br />
-          wie die Big Player <span className="ml-1 mb-3 inline-block h-2 w-20 bg-white" />
-          <br />
+          <br className="hidden md:block" /> wie die Big Player
+          <span className="mx-2 mb-2 inline-block h-1 w-12 bg-white sm:mb-2.5 sm:h-1.5 sm:w-14 lg:mx-3 lg:mb-3 lg:h-2 lg:w-20" />
+          <br className="hidden md:block" />
           ohne gleich ein Team einzustellen
+        </Heading>
+      </Section>
+      <Section className="pt-12">
+        <Heading level={2} className="max-w-4xl">
+          Das ist die Überschrift 2
+        </Heading>
+      </Section>
+      <Section className="pt-12">
+        <Heading level={3} className="max-w-4xl">
+          Das ist die Überschrift 3
+        </Heading>
+      </Section>
+      <Section className="pt-12">
+        <Heading level={4} className="max-w-4xl">
+          Das ist die Überschrift 4
         </Heading>
       </Section>
     </main>
--- components/atoms/heading.tsx
+++ components/atoms/heading.tsx
@@ -8,10 +8,10 @@ export const Heading: FC<{level: 1 | 2 | 3 | 4; size?: 'sm' | 'md' | 'lg' | 'xl'
 }) => {
   const style = 'text-white font-display font-extrabold leading-tight'
   const sizes = {
-    sm: 'text-xl drop-shadow-sm',
-    md: 'text-2xl drop-shadow',
-    lg: 'text-3xl drop-shadow-md',
-    xl: 'text-5xl drop-shadow-lg'
+    sm: 'text-lg lg:text-xl drop-shadow-sm',
+    md: 'text-xl lg:text-2xl drop-shadow',
+    lg: 'text-2xl lg:text-3xl drop-shadow-md',
+    xl: 'text-3xl sm:text-4xl lg:text-5xl drop-shadow-lg'
   }
   if (level === 1) return <h1 className={`${style} ${sizes[size ?? 'xl']} ${className}`}>{children}</h1>
   if (level === 2) return <h2 className={`${style} ${sizes[size ?? 'lg']} ${className}`}>{children}</h2>
