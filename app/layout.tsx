38eaf89e20c5d3e08cb4fd5cdb9fd100c564ff80
Lukas Wiesehan <mail@lukaswiesehan.de>
2022-12-25 13:02:30 +0100

    Tweak cards in light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
 import {Navigation} from '@components/layout/navigation'
-import {BlobLarge} from '@components/visual/svg-blobs/blob-large'
-import {Ellipse} from '@components/visual/svg-blobs/ellipse'
-import {Triangle} from '@components/visual/svg-blobs/triangle'
 import 'server-only'
 import './globals.css'
 import localFont from '@next/font/local'
 import Script from 'next/script'
+import {BlurBackground} from '@components/layout/blur-background'
 
 const lato = localFont({
   variable: '--font-lato',
@@ -63,12 +61,7 @@ export const Layout = async ({children}: {children: React.ReactNode}) => {
             `
           }}
         />
-        <div className="absolute inset-0 overflow-x-hidden 2xl:inset-x-64 2xl:overflow-x-visible">
-          <BlobLarge className="absolute -right-8 -top-8 w-1/2 animate-spin-slow text-[#AAACFF] blur-3xl dark:text-[#8182C8] md:-right-48 md:-top-40 md:blur-4xl" />
-          <Ellipse className="absolute left-32 -top-4 w-7/12 rotate-6 text-[#DCBAFF] blur-3xl dark:text-[#AA8FC5] md:-top-32 md:left-auto md:right-16 md:blur-4xl" />
-          <Ellipse className="absolute left-32 top-12 w-5/12 animate-spin-slow text-[#FFB800] mix-blend-overlay blur-2xl dark:text-[#A2B976] md:-top-8 md:left-1/4 md:blur-4xl" />
-          <Triangle className="absolute -top-12 right-12 w-5/12 animate-spin-slow text-[#47FFAA] mix-blend-overlay blur-2xl dark:text-[#B0F4D5] md:-top-36 md:opacity-80 md:blur-4xl" />
-        </div>
+        <BlurBackground />
         <Navigation />
         <div className="relative">{children}</div>
       </body>
--- components/atoms/card.tsx
+++ components/atoms/card.tsx
@@ -8,13 +8,16 @@ export const Card: FC<{glowColor?: string; className?: string; children: ReactNo
       >
         <div className="absolute inset-0 bg-gradient-radial from-transparent via-transparent to-slate-300 opacity-10 dark:to-slate-400" />
         <div
-          className="absolute left-1/4 right-1/4 -bottom-12 aspect-square scale-110 rounded-full opacity-25 blur-2xl lg:scale-100 lg:opacity-40 lg:blur-3xl lg:transition-all lg:duration-500 lg:group-hover:scale-125 lg:group-hover:opacity-60"
+          className="saturate-125 absolute left-1/4 right-1/4 -bottom-12 aspect-square scale-110 rounded-full opacity-25 blur-2xl brightness-125 dark:brightness-100 dark:saturate-100 lg:scale-100 lg:opacity-40 lg:blur-3xl lg:transition-all lg:duration-500 lg:group-hover:scale-125 lg:group-hover:opacity-60"
           style={{backgroundColor: glowColor}}
         />
         <div className="relative">{children}</div>
       </div>
       <div className="relative mx-8 -mt-px h-px overflow-hidden">
-        <div className="mx-auto -mt-6 h-12 w-2/3 blur-lg" style={{backgroundColor: glowColor}} />
+        <div
+          className="saturate-125 mx-auto -mt-6 h-12 w-2/3 blur-lg brightness-125 dark:brightness-100 dark:saturate-100"
+          style={{backgroundColor: glowColor}}
+        />
       </div>
     </div>
   )
