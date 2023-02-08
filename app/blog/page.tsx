1165a6fab2aab63d71559950e4394596aacdc5ac
Lukas Wiesehan <mail@lukaswiesehan.de>
2023-02-08 08:26:30 +0100

    Fix blog page paddigns

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,7 +6,7 @@ import {allBlogPosts} from 'contentlayer/generated'
 export default async function Blog() {
   return (
     <main>
-      <Section className="pt-32 pb-16 md:pt-40 md:pb-32 lg:pt-48">
+      <Section className="pt-32 pb-20 sm:pb-28 md:pb-32 md:pt-40 lg:pt-48 lg:pb-40 xl:pb-48">
         <Heading level={1}>Blog</Heading>
         <div className="mt-24 grid max-w-xl grid-cols-1 gap-16 md:max-w-none md:grid-cols-2">
           {allBlogPosts
